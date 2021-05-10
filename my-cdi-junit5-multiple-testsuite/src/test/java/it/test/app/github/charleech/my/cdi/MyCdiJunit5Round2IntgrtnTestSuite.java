5e075359eb0dba13a228e33fa475ac0f76358c8c
Charlee Chitsuk <charlee.ch@gmail.com>
2021-05-10 08:40:35 +0700

    #10 Use the JUnit 5 @Suite.
    - Use the JUnit 5 @Suite.

--- my-cdi-junit5-multiple-testsuite/src/test/java/it/test/app/github/charleech/my/cdi/MyCdiJunit5Round2IntgrtnTestSuite.java
+++ my-cdi-junit5-multiple-testsuite/src/test/java/it/test/app/github/charleech/my/cdi/MyCdiJunit5Round2IntgrtnTestSuite.java
@@ -1,10 +1,9 @@
 package it.test.app.github.charleech.my.cdi;
 
-import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.IncludeClassNamePatterns;
 import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
-import org.junit.runner.RunWith;
 
 /**
  * <p>
@@ -16,7 +15,7 @@ import org.junit.runner.RunWith;
  * @version 1.0.0
  * @since 1.0.0
  */
-@RunWith(JUnitPlatform.class)
+@Suite
 @SuiteDisplayName("JUnit Platform Suite Demo")
 @SelectClasses({
     MyCdiJunit5IntgrtnTester.class
